826635e7b158d55aaf8bf987a9878842fa9dc0d4
scc <songchengcheng@baijiahulian.com>
2019-10-14 00:17:20 +0800

    mock

---
+++ src/page/child.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    子组件：<span>{{inputName}}</span>
+    <div>
+      <input
+        type="button"
+        value="给父组件传值"
+        @click="childClick"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    // 接收父组件的值,props里面值不能改变
+    props: { // 对象写法
+        inputName: {
+            type: String,
+            require: true
+        }
+    },
+    data () {
+        return {
+            childValue: '我是子组件的数据'
+        };
+    },
+    methods: {
+        childClick () {
+            this.$emit('update:inputName', this.childValue);
+        }
+    }
+};
+</script>
---
+++ src/page/father.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    父组件:<input
+      v-model="inputName"
+      type="text" >
+    <child :input-name.sync="inputName"/>
+  </div>
+</template>
+
+<script>
+import child from '@/page/child';
+export default {
+    components: {
+        child
+    },
+    data () {
+        return {
+            inputName: ''
+        };
+    }
+};
+</script>
